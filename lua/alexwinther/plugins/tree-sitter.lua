3d2fff0b2a3e2f945e8b28838c1b6fa6a93df018
Alexander Winther Nielsen <alwn@itu.dk>
2023-09-22 18:30:55 +0200

    Styling: consistent indent and style

--- lua/alexwinther/plugins/tree-sitter.lua
+++ lua/alexwinther/plugins/tree-sitter.lua
@@ -1,29 +1,29 @@
 return {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-    config = function()
-        require('nvim-treesitter.configs').setup {
-            -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = { 'c', 'cpp', 'lua', 'python', 'vimdoc', 'vim', 'java' },
+	-- Highlight, edit, and navigate code
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- Add languages to be installed here that you want installed for treesitter
+			ensure_installed = { "c", "cpp", "lua", "python", "vimdoc", "vim", "java" },
 
-            -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-            auto_install = false,
+			-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+			auto_install = false,
 
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = '<c-space>',
-                    node_incremental = '<c-space>',
-                    scope_incremental = '<c-s>',
-                    node_decremental = '<M-space>',
-                },
-            },
-        }
-    end
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<c-space>",
+					node_incremental = "<c-space>",
+					scope_incremental = "<c-s>",
+					node_decremental = "<M-space>",
+				},
+			},
+		})
+	end,
 }
