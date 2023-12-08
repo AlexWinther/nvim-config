866560530b8f574bc880b6193be4d00e9f48a786
Alexander Winther Nielsen <alwn@itu.dk>
2023-12-08 22:58:04 +0100

    Fix: Capitalization of lua require statements

--- lua/alexwinther/lazy.lua
+++ lua/alexwinther/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "alexwinther.plugins" }, { import = "alexwinther.plugins.lsp" } }, {
+require("lazy").setup({ { import = "AlexWinther.plugins" }, { import = "AlexWinther.plugins.lsp" } }, {
 	checker = {
 		enabled = true,
 		notify = false,
