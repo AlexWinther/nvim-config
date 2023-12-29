0726b2dfd5be18fa96b85e4913da0c906a60169b
Alexander Winther Nielsen <alwn@itu.dk>
2023-12-30 00:04:04 +0100

    remap: change parameter swapping mapping

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -64,10 +64,10 @@ return {
                     swap = {
                         enable = true,
                         swap_next = {
-                            ['<leader>a'] = '@parameter.inner',
+                            ['<leader>p'] = '@parameter.inner',
                         },
                         swap_previous = {
-                            ['<leader>A'] = '@parameter.inner',
+                            ['<leader>P'] = '@parameter.inner',
                         },
                     },
                 },
