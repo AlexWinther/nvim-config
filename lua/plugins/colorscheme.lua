49db2daa33f0bb525a500dc13deb8cf17bf03bbd
Alexander Winther Nielsen <alwn@itu.dk>
2023-12-30 01:24:22 +0100

    color: change to rose-pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,23 @@
 return {
-    { "folke/tokyonight.nvim" },
-    { "rose-pine/neovim" },
     {
-        -- Theme inspired by Atom
-        'catppuccin/nvim',
+        'rose-pine/neovim',
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme 'catppuccin'
+            vim.cmd.colorscheme 'rose-pine'
         end,
+    },
+    {
+        'folke/tokyonight.nvim',
+        -- priority = 1000,
+        -- config = function()
+        --     vim.cmd.colorscheme 'folke/tokyonight.nvim'
+        -- end,
+    },
+    {
+        'catppuccin/nvim',
+        -- priority = 1000,
+        -- config = function()
+        --     vim.cmd.colorscheme 'catppuccin'
+        -- end,
     },
 }
